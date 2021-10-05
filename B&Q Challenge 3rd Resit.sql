233bcef0e0ed733764790c65c31bdc0fdf3d26be
quidang <103585238@student.swin.edu.au>
2021-10-05 12:35:09 +1100

    first commit

---
+++ B&Q Challenge 3rd Resit.sql
@@ -0,0 +1,41 @@
+-- STUDENT ID: 103585238 NAME: HUYNH QUI DANG 
+
+-- CREATE GITHUB REPOSITORY FOR BUILD AND QUERY CHALLENGE. 
+
+-- CONNECT TO DATABASE. 
+-- USE SEM2DB. 
+
+-- TASK 1. Convert ERD to Relational Schema. 
+-- TASK 2. Write and Execute the DDL to create your DB.  
+    -- Successfully create tables from the DD provided.
+    -- Confirm that tables were created via Select * From (Table Names).
+-- TASK 3. Adding Test Data to Tables. 
+-- TASK 4. Write and run queries to meet each of the following requirements. 
+    -- 
+                
+-- TASK 5. 
+
+-- SQL EXAMPLES: 
+
+-- CREATE TABLE table_name (       
+--     column1 datatype,
+--     column2 datatype,
+--     column3 datatype,
+--    ....
+-- );
+
+-- CREATE TABLE Persons (
+--     PersonID int,
+--     LastName varchar(255),
+--     FirstName varchar(255),
+--     Address varchar(255),
+--     City varchar(255)
+-- );
+
+-- SYNTAX: 
+-- CREATE TABLE new_table_name AS
+--     SELECT column1, column2,...
+--     FROM existing_table_name
+--     WHERE ....;
+
+
